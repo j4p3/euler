d33e917c791e9dc637a3485aa32be3868dce15c0
JP <jp.bonner@bonner.jp>
2021-11-07 01:56:03 -0500

    6

--- lib/Six.ex
+++ lib/Six.ex
@@ -6,4 +6,12 @@ defmodule Six do
 
   Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
   """
+
+  @doc """
+  Ideas:
+  * this is a trivial problem, given the constraints?
+  """
+  def run(limit) do
+    :math.pow(Enum.reduce(1..limit, fn i, sum -> sum + i end), 2) - Enum.reduce(1..limit, fn i, sum -> sum + :math.pow(i, 2) end)
+  end
 end
